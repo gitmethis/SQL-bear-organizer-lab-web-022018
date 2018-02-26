17166dc8ac75c207eec7c071dc6ff15106f2a871
Chase <chaseit12@gmail.com>
2018-02-26 15:17:56 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -23,3 +23,6 @@ VALUES ('Wendy', 6, 'F', 'red', 'stuff', 'yes');
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ('Unnamed', 99, 'M', 'red', 'stuff', 'yes');
+
+
+INSERT INTO bears VALUES (8,null, 20, "M", "black", "aggressive", 0);
