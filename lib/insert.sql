c1b317b3f7cd934bd6cd4dd63f4549cb3a163c1a
Chase <chaseit12@gmail.com>
2018-02-26 15:17:04 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,25 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Mr. Chocolate', 99, 'M', 'red', 'stuff', 'yes');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Rowdy', 99, 'M', 'red', 'stuff', 'yes');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Tabitha', 6, 'F', 'red', 'stuff', 'yes');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Sergeant Brown', 99, 'M', 'red', 'stuff', 'yes');
+
+
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Melissa', 13, 'F', 'red', 'stuff', 'yes');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Grinch', 99, 'M', 'red', 'stuff', 'yes');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Wendy', 6, 'F', 'red', 'stuff', 'yes');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Unnamed', 99, 'M', 'red', 'stuff', 'yes');
